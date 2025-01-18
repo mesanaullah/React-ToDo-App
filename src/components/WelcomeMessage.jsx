1bfc76f6427d422229c977409cb30916d25076a2
mesanaullah <mdsanaullah1227@gmail.com>
2025-01-18 17:33:33 +0530

    3rd Push

--- src/components/WelcomeMessage.jsx
+++ src/components/WelcomeMessage.jsx
@@ -7,7 +7,6 @@ const WelcomeMessage = () => {
     <div className = 'WelcomeMessage'>
       All Tasks are complete
       Enjoy the rest of your day!😊
-      console.log("Hello Guys");
     </div>
   )
 }
